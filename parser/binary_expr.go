60be7413bdf9f8c09826d2b0923dd9554cb1dcb6
Ethan <emh419@hotmail.com>
2024-11-01 02:37:02 -0500

    binary expression parsing using Operator-Precedence Parsing Algorithm

--- parser/binary_expr.go
+++ parser/binary_expr.go
@@ -1,7 +1,24 @@
 package parser
 
+var BinaryOpPrecedence = map[string]int{
+	"<": 10,
+	"+": 20,
+	"-": 20,
+	"*": 40,
+	"/": 40,
+}
+
 type BinaryExprNode struct {
 	operator string
 	LHS *ExprNode
 	RHS *ExprNode
+}
+
+// retrieves the precedence of a binary operator, -1 if it doesn't exist!
+func (parser *Parser) OperatorPrecedence(binop string) int {
+	precedence, ok := BinaryOpPrecedence[binop]
+	if !ok {
+		return -1
+	}
+	return precedence
 }
--- parser/expr.go
+++ parser/expr.go
@@ -1,6 +1,8 @@
 package parser
 
-import "github.com/ethan-prime/graphite/tokens"
+import (
+	"github.com/ethan-prime/graphite/tokens"
+)
 
 // define base expr for all other exprs
 type Expr interface {}
@@ -9,8 +11,43 @@ type ExprNode struct {
 	Expr Expr
 }
 
+// parses an (arbitrarily long) expression
 func (parser *Parser) ParseExpression() *ExprNode {
-	return &ExprNode{}
+	LHS := parser.ParsePrimaryExpression()
+
+	// parse RHS, if it exists...
+	return parser.ParseBinaryOpRHS(0, LHS)
+}
+
+// Parses RHS of a binary expression.
+// Operator-Precedence Parsing Algorithm
+func (parser *Parser) ParseBinaryOpRHS(current_precedence int, LHS *ExprNode) *ExprNode {
+	cur_tok := parser.CurrentToken()
+
+	for { // recursively parse RHS
+		precedence := parser.OperatorPrecedence(cur_tok.Value)
+
+		if precedence < current_precedence {
+			return LHS
+		}
+
+		// so we must have a binary operator
+		binop := parser.CurrentToken()
+		parser.Advance()
+
+		RHS := parser.ParsePrimaryExpression()
+
+		next_precedence := parser.OperatorPrecedence(parser.CurrentToken().Value)
+		if precedence < next_precedence {
+			RHS = parser.ParseBinaryOpRHS(precedence + 1, RHS)
+		}
+
+		// merge LHS and RHS
+		LHS = &ExprNode{
+			Expr: &BinaryExprNode{operator: binop.Value, LHS: LHS, RHS: RHS},
+		}
+
+	}
 }
 
 // parses a parenexpr
